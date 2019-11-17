b733b15c1746d56f4cbf24e4a05143edc7acd0d3
Jan Heuermann <jan@jotaen.net>
2019-11-17 18:32:15 +0100

    Improve API usability

--- src/Actor.h
+++ src/Actor.h
@@ -9,20 +9,60 @@ class Actor: public Device {
   typedef void (*Handler)();
 
 public:
-  Actor(Predicate p, Handler h)
+  Actor(Predicate p)
   : predicate(p)
-  , handler(h)
+  , hasLastState(false)
+  , lastState(false)
+  , ontrue(0)
+  , onfalse(0)
+  , whiletrue(0)
+  , whilefalse(0)
   {}
+
+  void onTrue(Handler h) {
+    this->ontrue = h;
+  }
+
+  void onFalse(Handler h) {
+    this->onfalse = h;
+  }
+
+  void whileTrue(Handler h) {
+    this->whiletrue = h;
+  }
+
+  void whileFalse(Handler h) {
+    this->whilefalse = h;
+  }
 
 protected:
   void onLoop() {
-    if (this->predicate()) {
-      this->handler();
+    bool currentState = this->predicate();
+    if (hasStateChanged(currentState)) {
+      invoke(currentState ? this->ontrue : this->onfalse);
+    }
+    invoke(currentState ? this->whiletrue : this->whilefalse);
+    this->lastState = currentState;
+    this->hasLastState = true;
+  }
+
+  bool hasStateChanged(bool newState) {
+    return this->hasLastState && (this->lastState != newState);
+  }
+
+  void invoke(Handler h) {
+    if (h != 0) {
+      h();
     }
   }
 
+  bool hasLastState;
+  bool lastState;
   Predicate predicate;
-  Handler handler;
+  Handler ontrue;
+  Handler onfalse;
+  Handler whiletrue;
+  Handler whilefalse;
 };
 
 #endif
--- src/Actor.spec.h
+++ src/Actor.spec.h
@@ -3,26 +3,85 @@
 #include <utils.h>
 #include "Actor.h"
 
+bool predicateValue = false;
+
 TEST_CASE("[Actor]") {
 
   callspy::reset();
   World::destroy();
+  predicateValue = false;
 
   SECTION("An Actor is a Device") {
-    Actor a = Actor(alwaysTrue, noop);
+    Actor a = Actor(alwaysTrue);
     Device& d = a; // The “assertion” is that this compiles
   }
 
-  SECTION("`onLoop` calls handler when predicate is `true`") {
-    Actor a = Actor(alwaysTrue, callspy::Void);
-    World::flush();
-    REQUIRE(callspy::hasBeenCalled());
+  SECTION("Absence of handlers is dealt with gracefully") {
+    Actor a = Actor([](){ return predicateValue; });
+    REQUIRE_NOTHROW(World::flush());
+    predicateValue = true;
+    REQUIRE_NOTHROW(World::flush());
   }
 
-  SECTION("`onLoop` doesn’t call handler when predicate is `false`") {
-    Actor a = Actor(alwaysFalse, callspy::Void);
-    World::flush();
-    REQUIRE(!callspy::hasBeenCalled());
+  SECTION("`onTrue` gets called once when state changes") {
+    predicateValue = true;
+    Actor a = Actor([](){ return predicateValue; });
+    a.onTrue(callspy::Void);
+    World::elapseMillis(10);
+    REQUIRE(callspy::hasBeenCalled() == false); // since there was no change so far
+    predicateValue = false;
+    World::elapseMillis(10);
+    REQUIRE(callspy::hasBeenCalled() == false);
+    predicateValue = true;
+    World::elapseMillis(10);
+    REQUIRE(callspy::counter() == 1);
+  }
+
+  SECTION("`whileTrue` gets called repeatedly") {
+    Actor a = Actor(alwaysTrue);
+    a.whileTrue(callspy::Void);
+    World::elapseMillis(10);
+    REQUIRE(callspy::hasBeenCalled());
+    REQUIRE(callspy::counter() > 2);
+  }
+
+  SECTION("`onFalse` gets called once") {
+    Actor a = Actor([](){ return predicateValue; });
+    a.onFalse(callspy::Void);
+    World::elapseMillis(10);
+    REQUIRE(callspy::hasBeenCalled() == false); // since there was no change so far
+    predicateValue = true;
+    World::elapseMillis(10);
+    REQUIRE(callspy::hasBeenCalled() == false);
+    predicateValue = false;
+    World::elapseMillis(10);
+    REQUIRE(callspy::counter() == 1);
+  }
+
+  SECTION("`whileFalse` gets called repeatedly") {
+    Actor a = Actor(alwaysFalse);
+    a.whileFalse(callspy::Void);
+    World::elapseMillis(10);
+    REQUIRE(callspy::hasBeenCalled());
+    REQUIRE(callspy::counter() > 2);
+  }
+
+  SECTION("For `true`: Only the matching handlers get invoked") {
+    Actor a = Actor(alwaysTrue);
+    a.onTrue(callspy::Void);
+    a.whileTrue(callspy::Void);
+    a.onFalse([](){ throw; });
+    a.whileFalse([](){ throw; });
+    REQUIRE_NOTHROW(World::flush());
+  }
+
+  SECTION("For `false`: Only the matching handlers get invoked") {
+    Actor a = Actor(alwaysFalse);
+    a.onFalse(callspy::Void);
+    a.whileFalse(callspy::Void);
+    a.onTrue([](){ throw; });
+    a.whileTrue([](){ throw; });
+    REQUIRE_NOTHROW(World::flush());
   }
 
 }
