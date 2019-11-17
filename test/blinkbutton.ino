733e2722f20b3c7b733b36de1dd9e82e939c812d
Jan Heuermann <jan@jotaen.net>
2019-11-17 10:36:28 +0100

    Split spec from sketch

---
+++ test/blinkbutton.ino
@@ -0,0 +1,19 @@
+#include "../yps.h"
+
+DigitalInput button = DigitalInput(4);
+
+bool led = false;
+
+unsigned long frequency = 100;
+
+Timer ledBlinker = Timer([](){ led = !led; });
+
+Actor onButtonHigh = Actor(
+  []()->bool { return button.isHigh(); },
+  [](){ if (!ledBlinker.isActive()) {ledBlinker.runMillis(frequency);} }
+);
+
+Actor onButtonLow = Actor(
+  []()->bool { return button.isLow(); },
+  [](){ ledBlinker.stop(); led = false; }
+);
--- test/blinkbutton.spec.cpp
+++ test/blinkbutton.spec.cpp
@@ -1,19 +1,5 @@
-#define CATCH_CONFIG_MAIN
-#include "../test.h"
-#include "../yps.h"
-
-DigitalInput button = DigitalInput(4);
-bool led = false;
-unsigned long frequency = 100;
-Timer ledBlinker = Timer([](){ led = !led; });
-Actor onButtonHigh = Actor(
-  []()->bool { return button.isHigh(); },
-  [](){ if (!ledBlinker.isActive()) {ledBlinker.runMillis(frequency);} }
-);
-Actor onButtonLow = Actor(
-  []()->bool { return button.isLow(); },
-  [](){ ledBlinker.stop(); led = false; }
-);
+#include "yps-test.h"
+#include "blinkbutton.ino"
 
 TEST_CASE("LED starts blinking while button is pressed", "[BlinkButton]") {
   World::setDigitalInput(4, HIGH);
