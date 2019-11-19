2b5c9eff320d71f6c454b2c649f336cfb0023f58
Jan Heuermann <jan@jotaen.net>
2019-11-19 08:31:12 +0100

    Implement 3 basic log levels

--- lib/virtuino/Virtuino.h
+++ lib/virtuino/Virtuino.h
@@ -78,20 +78,50 @@ uint8_t digitalPinToPort(uint8_t pin) {
 }
 
 struct SoftwareSerial {
+  static const int DEC = 10;
+
   void begin(long speed);
+
   size_t print(const String &);
+  size_t print(const char[]);
+  // size_t print(char);
+  // size_t print(unsigned char, int = DEC);
+  // size_t print(int, int = DEC);
+  // size_t print(unsigned int, int = DEC);
+  // size_t print(long, int = DEC);
+  // size_t print(unsigned long, int = DEC);
+  // size_t print(double, int = 2);
+
   size_t println(const String &s);
+  size_t println(const char[]);
+  // size_t println(char);
+  // size_t println(unsigned char, int = DEC);
+  // size_t println(int, int = DEC);
+  // size_t println(unsigned int, int = DEC);
+  // size_t println(long, int = DEC);
+  // size_t println(unsigned long, int = DEC);
+  // size_t println(double, int = 2);
+  // size_t println(void);
 } Serial;
 
-void SoftwareSerial::begin(long speed) {
+void SoftwareSerial::begin(long speed) {}
 
-}
-
-size_t SoftwareSerial::print(const String &s) {
+size_t SoftwareSerial::print(const String& s) {
+  std::cout << s;
   return 0;
 }
 
-size_t SoftwareSerial::println(const String &s) {
+size_t SoftwareSerial::print(const char* s) {
+  std::cout << s;
+  return 0;
+}
+
+size_t SoftwareSerial::println(const String& s) {
+  std::cout << s << std::endl;
+  return 0;
+}
+
+size_t SoftwareSerial::println(const char* s) {
   std::cout << s << std::endl;
   return 0;
 }
--- src/util/logger.h
+++ src/util/logger.h
@@ -2,7 +2,6 @@
 #define __ESPRESSO_DEBUG_H__
 
 #include <Arduino.h>
-#include <memory>
 
 namespace logger {
 
@@ -11,11 +10,36 @@ namespace logger {
   }
 
   template<class T>
-  void info(const char* message, T* origin) {
-    std::cout << std::addressof(origin) << std::endl;
-    Serial.println(message);
+  String addressToString(T* ptr) {
+    std::ostringstream address;
+    address << (void const *)ptr;
+    return address.str();
   }
 
+  template<class T>
+  void log(const char* level, T* origin, const char* message) {
+    Serial.print(level);
+    Serial.print(" [");
+    Serial.print(addressToString(origin));
+    Serial.print("]: ");
+    Serial.print(message);
+    Serial.println("");
+  }
+
+  template<class T>
+  void info(T* origin, const char* message) {
+    log("INFO", origin, message);
+  }
+
+  template<class T>
+  void warning(T* origin, const char* message) {
+    log("WARNING", origin, message);
+  }
+
+  template<class T>
+  void error(T* origin, const char* message) {
+    log("ERROR", origin, message);
+  }
 }
 
 #endif
