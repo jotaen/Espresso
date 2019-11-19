dfd6ccac732f3298a5f3289fd6741c0bcdd9d615
Jan Heuermann <jan@jotaen.net>
2019-11-19 07:36:54 +0100

    “Blacklist” original functions

--- Espresso.h
+++ Espresso.h
@@ -1,6 +1,10 @@
 #ifndef __ESPRESSO_H__
 #define __ESPRESSO_H__
 
+#define pinMode _BLACKLISTED_pinMode_
+#define digitalWrite _BLACKLISTED_digitalWrite_
+#define digitalRead _BLACKLISTED_digitalRead_
+
 #include <Arduino.h>
 
 #include "src/Observer.h"
