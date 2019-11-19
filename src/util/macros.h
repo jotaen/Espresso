833cb77622cfff4ca6f5cb6a2c58a5bf17803ea0
Jan Heuermann <jan@jotaen.net>
2019-11-19 11:55:35 +0100

    Fix reference error

--- src/util/macros.h
+++ src/util/macros.h
@@ -3,5 +3,5 @@
     if (!bool_expression) { throw error_message; }
 #else
   #define ASSERT( bool_expression, error_message ) \
-    if (!bool_expression) { log::error(error_message); exit(1); }
+    if (!bool_expression) { logger::error(error_message); exit(1); }
 #endif
