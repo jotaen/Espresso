baed4d926b063e9ab1619b8a0bd654721bf26d49
Jan Heuermann <jan@jotaen.net>
2019-11-20 23:00:42 +0100

    Optimise benchmark setup

--- test/performance-tests/Observer/Observer.ino
+++ test/performance-tests/Observer/Observer.ino
@@ -7,12 +7,12 @@ Observer observer([](){ return observerState; });
 
 Test test = Test("Observer")
     .sizeOf<Observer>(14)
-    .benchmark("update()", 5.38*m, [](){ observer.update(); });
+    .benchmark("update()", 5.16*m, [](){ observer.update(); });
 
 void setup() {
   observerState = true; // change to trigger observer initially
-  observer.onTrue([](){ observerState = !observerState; });
-  observer.onFalse([](){ observerState = !observerState; });
+  observer.onTrue([](){ observerState = false; });
+  observer.onFalse([](){ observerState = true; });
 }
 
 void loop() {
