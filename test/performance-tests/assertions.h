9e60c0bb90d0b8970c7fdec37356ccbb038f2e33
Jan Heuermann <jan@jotaen.net>
2019-11-20 21:29:13 +0100

    Decrease error margin

--- test/performance-tests/assertions.h
+++ test/performance-tests/assertions.h
@@ -1,13 +1,13 @@
-DigitalInput di(2);
-DigitalOutput dt(4);
+DigitalInput digitalInput(2);
+DigitalOutput digitalOutput(4);
 
 bool observerState = false;
-Observer o([](){ return observerState; });
+Observer observer([](){ return observerState; });
 
 void setup() {
   observerState = true; // change to trigger observer initially
-  o.onTrue([](){ observerState = !observerState; });
-  o.onFalse([](){ observerState = !observerState; });
+  observer.onTrue([](){ observerState = !observerState; });
+  observer.onFalse([](){ observerState = !observerState; });
 }
 
 const double m = 1.03; // 3% margin for performance benchmarks
@@ -15,16 +15,16 @@ Test tests[] = {
 
   Test("DigitalInput")
     .sizeOf<DigitalInput>(4)
-    .benchmark("value()", 1.45*m, [](){ di.value(); })
+    .benchmark("value()", 1.45*m, [](){ digitalInput.value(); })
 
   ,Test("DigitalOutput")
     .sizeOf<DigitalOutput>(6)
-    .benchmark("write()", 2.46*m, [](){ dt.write(HIGH); })
-    .benchmark("toggle()", 3.11*m, [](){ dt.toggle(); })
+    .benchmark("write()", 2.46*m, [](){ digitalOutput.write(HIGH); })
+    .benchmark("toggle()", 3.11*m, [](){ digitalOutput.toggle(); })
 
   ,Test("Observer")
     .sizeOf<Observer>(14)
-    .benchmark("update()", 5.38*m, [](){ o.update(); })
+    .benchmark("update()", 5.63*m, [](){ observer.update(); })
 
   ,Test("Metronome")
     .sizeOf<Metronome>(15)
