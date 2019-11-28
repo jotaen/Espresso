357f38272ea2a351372705ebd44ad5b80ca6972b
Jan Heuermann <jan@jotaen.net>
2019-11-28 13:53:35 +0100

    Adjust test structure

--- test/integration-tests/button-led/button-led.spec.cpp
+++ test/integration-tests/button-led/button-led.spec.cpp
@@ -3,44 +3,47 @@
 #include <Virtuino.h>
 #include "button-led.ino"
 
-Virtuino virtuino;
+TEST_CASE("[Button-LED]") {
 
-TEST_CASE("LED is off in the beginning", "[Button-LED]") {
-  REQUIRE(led.isLow());
-  virtuino.elapseMillis(duration);
-  REQUIRE(led.isLow());
-  virtuino.elapseMillis(duration);
-  REQUIRE(led.isLow());
-}
+  Virtuino virtuino(Virtuino::RESET);
+  
+  SECTION("LED is off in the beginning") {
+    REQUIRE(led.isLow());
+    virtuino.elapseMillis(duration);
+    REQUIRE(led.isLow());
+    virtuino.elapseMillis(duration);
+    REQUIRE(led.isLow());
+  }
 
-TEST_CASE("LED is lighten up for 3 seconds when is pressed", "[Button-LED]") {
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  REQUIRE(led.isHigh());
-  virtuino.elapseMillis(duration);
-  REQUIRE(led.isLow());
-  virtuino.elapseMillis(duration);
-  REQUIRE(led.isLow());
-}
+  SECTION("LED is lighten up for 3 seconds when is pressed") {
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    REQUIRE(led.isHigh());
+    virtuino.elapseMillis(duration);
+    REQUIRE(led.isLow());
+    virtuino.elapseMillis(duration);
+    REQUIRE(led.isLow());
+  }
 
-TEST_CASE("Keeping the button pressed switches the LED off anyway", "[Button-LED]") {
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  REQUIRE(led.isHigh());
-  virtuino.elapseMillis(duration);
-  REQUIRE(led.isLow());
-  virtuino.setDigitalInput(button.pin(), LOW);
-  virtuino.elapseMillis(duration);
-  REQUIRE(led.isLow());
-}
+  SECTION("Keeping the button pressed switches the LED off anyway") {
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    REQUIRE(led.isHigh());
+    virtuino.elapseMillis(duration);
+    REQUIRE(led.isLow());
+    virtuino.setDigitalInput(button.pin(), LOW);
+    virtuino.elapseMillis(duration);
+    REQUIRE(led.isLow());
+  }
 
-TEST_CASE("Pressing the button again extends the duration", "[Button-LED]") {
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  REQUIRE(led.isHigh());
-  virtuino.elapseMillis(duration * 0.5);
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  REQUIRE(led.isHigh());
-  virtuino.elapseMillis(duration);
-  REQUIRE(led.isLow());
+  SECTION("Pressing the button again extends the duration") {
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    REQUIRE(led.isHigh());
+    virtuino.elapseMillis(duration * 0.5);
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    REQUIRE(led.isHigh());
+    virtuino.elapseMillis(duration);
+    REQUIRE(led.isLow());
+  }
 }
--- test/integration-tests/traffic-lights/traffic-lights.spec.cpp
+++ test/integration-tests/traffic-lights/traffic-lights.spec.cpp
@@ -3,72 +3,70 @@
 #include <Virtuino.h>
 #include "traffic-lights.ino"
 
-Virtuino virtuino;
+TEST_CASE("[Traffic-Lights]") {
 
-TEST_CASE("Traffic light is green initially", "[Traffic-Lights]") {
-  REQUIRE(red.isLow());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isHigh());
-}
+  Virtuino virtuino(Virtuino::RESET);
 
-TEST_CASE("Traffic light switches to red in correct sequence", "[Traffic-Lights]") {
-  REQUIRE(red.isLow());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isHigh());
-  virtuino.elapseMillis(5000);
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  REQUIRE(red.isLow());
-  REQUIRE(yellow.isHigh());
-  REQUIRE(green.isLow());
-  virtuino.elapseMillis(1000);
-  REQUIRE(red.isHigh());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isLow());
-}
+  SECTION("Traffic light is green initially") {
+    REQUIRE(red.isLow());
+    REQUIRE(yellow.isLow());
+    REQUIRE(green.isHigh());
+  }
 
-TEST_CASE("Traffic light switches to green in correct sequence", "[Traffic-Lights]") {
-  REQUIRE(red.isHigh());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isLow());
-  virtuino.elapseMillis(5000);
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  REQUIRE(red.isHigh());
-  REQUIRE(yellow.isHigh());
-  REQUIRE(green.isLow());
-  virtuino.elapseMillis(1000);
-  REQUIRE(red.isLow());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isHigh());
-}
+  SECTION("Traffic light switches to red in correct sequence") {
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    REQUIRE(red.isLow());
+    REQUIRE(yellow.isHigh());
+    REQUIRE(green.isLow());
+    virtuino.elapseMillis(1000);
+    REQUIRE(red.isHigh());
+    REQUIRE(yellow.isLow());
+    REQUIRE(green.isLow());
+  }
 
-TEST_CASE("Button is deactivated while switching is ongoing", "[Traffic-Lights]") {
-  REQUIRE(red.isLow());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isHigh());
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  REQUIRE(red.isLow());
-  REQUIRE(yellow.isHigh());
-  REQUIRE(green.isLow());
-  virtuino.elapseMillis(900);
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  virtuino.elapseMillis(100);
-  REQUIRE(red.isHigh());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isLow());
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  REQUIRE(red.isHigh());
-  REQUIRE(yellow.isHigh());
-  REQUIRE(green.isLow());
-  virtuino.elapseMillis(900);
-  virtuino.setDigitalInput(button.pin(), HIGH);
-  virtuino.setDigitalInput(button.pin(), LOW);
-  virtuino.elapseMillis(100);
-  REQUIRE(red.isLow());
-  REQUIRE(yellow.isLow());
-  REQUIRE(green.isHigh());
+  SECTION("Traffic light switches to green in correct sequence") {
+    // Set to red first
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    virtuino.elapseMillis(5000);
+
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    REQUIRE(red.isHigh());
+    REQUIRE(yellow.isHigh());
+    REQUIRE(green.isLow());
+    virtuino.elapseMillis(1000);
+    REQUIRE(red.isLow());
+    REQUIRE(yellow.isLow());
+    REQUIRE(green.isHigh());
+  }
+
+  SECTION("Button is deactivated while switching is in progress") {
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    REQUIRE(red.isLow());
+    REQUIRE(yellow.isHigh());
+    REQUIRE(green.isLow());
+    virtuino.elapseMillis(900);
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    virtuino.elapseMillis(100);
+    REQUIRE(red.isHigh());
+    REQUIRE(yellow.isLow());
+    REQUIRE(green.isLow());
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    REQUIRE(red.isHigh());
+    REQUIRE(yellow.isHigh());
+    REQUIRE(green.isLow());
+    virtuino.elapseMillis(900);
+    virtuino.setDigitalInput(button.pin(), HIGH);
+    virtuino.setDigitalInput(button.pin(), LOW);
+    virtuino.elapseMillis(100);
+    REQUIRE(red.isLow());
+    REQUIRE(yellow.isLow());
+    REQUIRE(green.isHigh());
+  }
+
 }
