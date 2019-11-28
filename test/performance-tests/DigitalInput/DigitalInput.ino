29f0f8ecd8a16037c3bca88f7a4b1d78fb77d319
Jan Heuermann <jan@jotaen.net>
2019-11-28 20:47:10 +0100

    Add reference benchmark

--- test/performance-tests/DigitalInput/DigitalInput.ino
+++ test/performance-tests/DigitalInput/DigitalInput.ino
@@ -1,3 +1,4 @@
+#define ESPRESSO_NOBLACKLISTING
 #define ESPRESSO_INCLUDES_ONLY
 #include <Espresso.h>
 #include <test/performance-tests/lib.h>
@@ -6,6 +7,9 @@ DigitalInput digitalInput(2);
 
 Test test = Test("DigitalInput")
     .sizeOf<DigitalInput>(4)
+    .benchmark("REFERENCE digitalRead()", 3.96*m, [](){
+      digitalRead(1);
+    })
     .benchmark("value()", 1.45*m, [](){ digitalInput.value(); });
 
 void setup() {}
